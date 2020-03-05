66b146941c601062d11829548cfe8133e6801737
mauro Leos <mauroleos94@gmail.com>
2020-03-04 23:03:25 -0600

    Remove Items from list when button is clicked

--- scripts.js
+++ scripts.js
@@ -1,36 +1,47 @@
 const form = document.getElementById('list-container');
 const itemList = document.getElementById('items');
 form.addEventListener('submit', addItem);
-form.addEventListener('click', removeItem)
+// form.addEventListener('click', removeItem);
 
 let itemsArray = [];
 
 function addItem(e){
   e.preventDefault();
-
   const newItem = document.getElementById('item').value;
   const li = document.createElement('li');
-  li.className='list-item';
   const textNode = document.createTextNode(newItem);
   li.appendChild(textNode);
 
   const del = document.createElement('button');
-  del.className = 'dlt-button';
   del.appendChild(document.createTextNode('X'));
   li.appendChild(del);
 
+  const strikeLine = document.createElement('input');
+  strikeLine.setAttribute('type', 'checkbox');
+  strikeLine.setAttribute('id', 'checkboxLine')
 
   itemList.appendChild(li);
 
-  // itemsArray.push(newItem);
-  // localStorage.setItem('list', JSON.stringify(itemsArray))
-  // const data = localStorage.getItem('list');
-  // const returnArray = JSON.parse('data');
-  // console.log(returnArray);
-  // console.log(typeof(returnArray));
+  del.addEventListener('click',function(){
+    const liDel = document.getElementById('items');
+    liDel.removeChild(li);
+    const removeDelBtn = document.getElementById('items');
+    removeDelBtn.removeChild(del);
+    const removeStrikeBtn = document.getElementById('items');
+    removeStrikeBtn.removeChild(strikeLine);
+  });
+
+  document.getElementById('items').appendChild(li);
+  document.getElementById('items').appendChild(del);
+  document.getElementById('items').appendChild(strikeLine)
+
+  itemsArray.push(newItem);
+  localStorage.setItem('list', JSON.stringify(itemsArray));
 
 }
 
-function removeItem(e){
-  console.log(e.target);
-}
+
+
+//** A user should be able to mark a TO-DO as completed
+//** When a user mark’s a TO-DO as completed the message of the TO-DO should be crossed out (using strikethrough font)
+//** A user should have the option of deleting TO-DO’s that have been mark completed
