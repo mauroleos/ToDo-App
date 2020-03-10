be25ea3ca587d6033d22ec9ca93a236a9509203a
mauro Leos <mauroleos94@gmail.com>
2020-03-09 23:13:28 -0500

    Worked on localstorage

--- scripts.js
+++ scripts.js
@@ -43,40 +43,44 @@ function addItem(e){
 }
 
 //*** Displaying lis on website ***
-for (let i=0; i<list.length; i++){
-  const displayItems = list[i];
+const savedItems = JSON.parse(localStorage.getItem('list') || '[]');
+for (let i=0; i<savedItems.length; i++){
+  const displayItems = savedItems[i];
+  console.log(savedItems);
+
+  const newItem=document.getElementById('item').value;
+  const li=document.createElement('li');
+  const textNode=document.createTextNode(newItem);
+  li.appendChild(textNode);
+
+//*** Adding delete button to Lis ***
+  const del=document.createElement('button');
+  del.appendChild(document.createTextNode('X'));
+  li.appendChild(del);
+
+//*** Adding strikeLine to lis ***
+  const strikeLine=document.createElement('input');
+  strikeLine.setAttribute('type', 'checkbox');
+  strikeLine.setAttribute('id', 'checkboxLine')
+
+//*** Removing lis and buttons from Array ***
+  del.addEventListener('click',function(){
+    const liDel=document.getElementById('items');
+    liDel.removeChild(li);
+    const removeDelBtn=document.getElementById('items');
+    removeDelBtn.removeChild(del);
+    const removeStrikeBtn=document.getElementById('items');
+    removeStrikeBtn.removeChild(strikeLine);
+    localStorage.removeItem('list');
+  })
+
+  document.getElementById('items').appendChild(li);
+  document.getElementById('items').appendChild(del);
+  document.getElementById('items').appendChild(strikeLine)
 }
-
-const newItem=document.getElementById('item').value;
-const li=document.createElement('li');
-const textNode=document.createTextNode(newItem);
-li.appendChild(textNode);
-
-const del=document.createElement('button');
-del.appendChild(document.createTextNode('X'));
-li.appendChild(del);
-
-const strikeLine=document.createElement('input');
-strikeLine.setAttribute('type', 'checkbox');
-strikeLine.setAttribute('id', 'checkboxLine')
-
-
-del.addEventListener('click',function(){
-  const liDel=document.getElementById('items');
-  liDel.removeChild(li);
-  const removeDelBtn=document.getElementById('items');
-  removeDelBtn.removeChild(del);
-  const removeStrikeBtn=document.getElementById('items');
-  removeStrikeBtn.removeChild(strikeLine);
-  localStorage.removeItem('list');
-});
-
-document.getElementById('items').appendChild(li);
-document.getElementById('items').appendChild(del);
-document.getElementById('items').appendChild(strikeLine)
 
 
 //*** Remvoing lis from website works succesfully but:
-  //*** 1. All variables are reomoved from Array when the delete button is clicked
+  //*** 1. All variables are removed from Array when the delete button is clicked
   //*** 2. Reloading the page shows the delete button and the checkbox but not the li for only 1 item
   //*** 3. To-Dos fail to crossed out when the checkbox is clicked
