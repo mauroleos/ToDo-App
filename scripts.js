ff37e246ba5f07a956c4f0665a32b6039c078575
mauro Leos <mauroleos94@gmail.com>
2020-03-17 00:51:57 -0500

    Worked on removing items from array

--- scripts.js
+++ scripts.js
@@ -1,19 +1,25 @@
 const form=document.getElementById('list-container');
 form.addEventListener('submit', addItem);
 let itemsArray =[];
-const list=JSON.parse(localStorage.getItem('list') || itemsArray);
+const list=JSON.parse(localStorage.getItem('list')) || itemsArray;
+
+const itemId = list.length ? list.length : 0
+
+console.log(itemId)
+
 //*** Adding lis ***
 function addItem(e){
   e.preventDefault();
   const newItem=document.getElementById('item').value;
   const li=document.createElement('li');
-  li.setAttribute('id', 'listItems');
+  const liAttribute=li.setAttribute('id', itemId);
+  console.log(itemId)
   const textNode=document.createTextNode(newItem);
   li.appendChild(textNode);
   //*** Adding Strikethrough line to Li ***
   const strikeLine=document.createElement('input');
   strikeLine.setAttribute('type', 'checkbox');
-  strikeLine.setAttribute('id', 'checkboxLine')
+  strikeLine.setAttribute('class', 'checkboxLine')
   strikeLine.addEventListener('click',function(e){
     strikeLine.checked? e.target.parentElement.classList.add('strikeLine'): e.target.parentElement.classList.remove('strikeLine');
   });
@@ -24,15 +30,15 @@ function addItem(e){
   li.appendChild(del);
   //*** Removing items from array ***
   del.addEventListener('click',function(e){
-    const liDel=document.getElementById('items');
-    liDel.removeChild(li);
-    const deletedItemsArray=JSON.parse(localStorage.getItem('list') || '[]');
+    const deletedItemsArray=JSON.parse(localStorage.getItem('list')) || itemsArray;
     console.log(deletedItemsArray)
-    const indexItem=list.indexOf(e.target.parentElement.textContent);
-    console.log(indexItem);
+    const indexItem=deletedItemsArray.indexOf(newItem);
+    // console.log(indexItem);
+    // console.log(newItem)
     localStorage.removeItem(indexItem);
     localStorage.setItem('list', JSON.stringify(deletedItemsArray));
-    console.log(e)
+    // liDel.removeChild(li);
+    // console.log(e)
   })
   document.getElementById('items').appendChild(li);
   //*** Pushing items to localStorage ***
@@ -40,36 +46,36 @@ function addItem(e){
   localStorage.setItem('list', JSON.stringify(itemsArray));
 }
 
-let displayArrayItems = [];
-//*** Adding lis ***
-const savedItems = JSON.parse(localStorage.getItem('list') || displayArrayItems);
-for (let i=0; i<savedItems.length; i++){
-  const displayItems = savedItems[i];
-  const newItem=displayItems
-  const li=document.createElement('li');
-  li.setAttribute('id', 'listItems');
-  const textNode=document.createTextNode(newItem);
-  li.appendChild(textNode);
-  //*** Adding strikethrough line to lis ***
-  const strikeLine=document.createElement('input');
-  strikeLine.setAttribute('type', 'checkbox');
-  strikeLine.setAttribute('id', 'checkboxLine')
-  strikeLine.addEventListener('click',function(e){
-    strikeLine.checked? e.target.parentElement.classList.add('strikeLine'): e.target.parentElement.classList.remove('strikeLine');
-  });
-  li.appendChild(strikeLine);
-  //*** Adding delete button to Lis ***
-  const del=document.createElement('button');
-  del.appendChild(document.createTextNode('X'));
-  li.appendChild(del);
-  //*** Removing items from array ***
-  del.addEventListener('click',function(e){
-    const liDel=document.getElementById('items');
-    liDel.removeChild(li);
-    const deletedItemsArray=JSON.parse(localStorage.getItem('list') || '[]');
-    const indexItem=list.indexOf(e.target.parentElement.textContent);
-    localStorage.removeItem(indexItem);
-    localStorage.setItem('list', JSON.stringify(deletedItemsArray));
-  })
-  document.getElementById('items').appendChild(li);
-}
+// let displayArrayItems = [];
+// //*** Adding lis ***
+// const savedItems = JSON.parse(localStorage.getItem('list')) || displayArrayItems;
+// for (let i=0; i<savedItems.length; i++){
+//   const displayItems = savedItems[i];
+//   const newItem=displayItems
+//   const li=document.createElement('li');
+//   li.setAttribute('id', 'displayListItems');
+//   const textNode=document.createTextNode(newItem);
+//   li.appendChild(textNode);
+//   //*** Adding strikethrough line to lis ***
+//   const strikeLine=document.createElement('input');
+//   strikeLine.setAttribute('type', 'checkbox');
+//   strikeLine.setAttribute('class', 'checkboxLine')
+//   strikeLine.addEventListener('click',function(e){
+//     strikeLine.checked? e.target.parentElement.classList.add('strikeLine'): e.target.parentElement.classList.remove('strikeLine');
+//   });
+//   li.appendChild(strikeLine);
+//   //*** Adding delete button to Lis ***
+//   const del=document.createElement('button');
+//   del.appendChild(document.createTextNode('X'));
+//   li.appendChild(del);
+//   //*** Removing items from array ***
+//   del.addEventListener('click',function(e){
+//     const liDel=document.getElementById('items');
+//     liDel.removeChild(li);
+//     const deletedItemsArray=JSON.parse(localStorage.getItem('list')) || '[]';
+//     const indexItem=list.indexOf(e.target.parentElement.textContent);
+//     localStorage.removeItem(indexItem);
+//     localStorage.setItem('list', JSON.stringify(deletedItemsArray));
+//   })
+//   document.getElementById('items').appendChild(li);
+// }
