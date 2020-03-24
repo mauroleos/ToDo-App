babffbbd7dbb2ffe534a23215d926b1a9a456c91
mauro Leos <mauroleos94@gmail.com>
2020-03-24 00:36:01 -0500

    Updated JS

--- scripts.js
+++ scripts.js
@@ -28,31 +28,32 @@ function addItem(e){
   //*** Removing items from array ***
   del.addEventListener('click',function(e){
     const liDel=document.getElementById('items');
-    const deletedItemsArray=JSON.parse(localStorage.getItem('list')) || itemsArray;
-    console.log(deletedItemsArray)
-    const indexItem=deletedItemsArray.indexOf(e.target.parentElement);
+    const indexItem=list.indexOf(newItem);
+    list.splice(e.target.parentElement.innerText,1)
+    console.log(list)
     // console.log(e)
-    // console.log(e.target.parentElement)
+    console.log(e.target.parentElement.innerText)
     liDel.removeChild(li);
   })
   document.getElementById('items').appendChild(li);
-  //*** Pushing items to localStorage ***
   list.push(newItem);
   localStorage.setItem('list', JSON.stringify(list));
 }
-
-function deleteTask(id){
-  for(let i=0; i<deletedItemsArray.length; i++){
-    if(deletedItemsArray[i].id == itemId){
-      list.splice(i,1);
-    }
-    localStorage.setItem('list', JSON.stringify(list));
-    console.log(list);
-  }
-}
+// 
+// function deleteTask(id){
+//   const list=JSON.parse(localStorage.getItem('list')) || itemsArray;
+//   const itemId = list.length ? list.length : 0
+//   for(let i=0; i<list.length; i++){
+//     if(list[i].id == itemId){
+//       list.splice(i,1);
+//     }
+//     localStorage.setItem('list', JSON.stringify(list));
+//     console.log(list);
+//   }
+// }
 
 let displayArrayItems = [];
-//*** Adding lis ***
+//*** Adding items ***
 const savedItems = JSON.parse(localStorage.getItem('list')) || displayArrayItems;
 for (let i=0; i<savedItems.length; i++){
   const displayItems = savedItems[i]
@@ -60,7 +61,7 @@ for (let i=0; i<savedItems.length; i++){
   const li=document.createElement('li');
   const textNode=document.createTextNode(newItem);
   li.appendChild(textNode);
-  //*** Adding strikethrough line to lis ***
+  //*** Adding strikethrough line to items ***
   const strikeLine=document.createElement('input');
   strikeLine.setAttribute('type', 'checkbox');
   strikeLine.setAttribute('class', 'checkboxLine')
@@ -68,11 +69,11 @@ for (let i=0; i<savedItems.length; i++){
     strikeLine.checked? e.target.parentElement.classList.add('strikeLine'): e.target.parentElement.classList.remove('strikeLine');
   });
   li.appendChild(strikeLine);
-  //*** Adding delete button to Lis ***
+  //*** Adding delete button to items ***
   const del=document.createElement('button');
   del.appendChild(document.createTextNode('X'));
   li.appendChild(del);
-  //*** Gets rid of Li - Returns disappeared items after reload and allows to you to remove it again ***
+  //*** Gets rid of items - Returns disappeared items after reload and allows to you to remove it again ***
   del.addEventListener('click',function(e){
     const liDel=document.getElementById('items');
     liDel.removeChild(li);
