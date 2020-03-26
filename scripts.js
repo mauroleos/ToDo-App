6dddba0395caced65f5b416abcd7cdd5b45c1a17
mauro Leos <mauroleos94@gmail.com>
2020-03-26 00:16:04 -0500

    Worked on CSS

--- scripts.js
+++ scripts.js
@@ -9,51 +9,37 @@ function addItem(e){
   const newItem=document.getElementById('item').value;
   const li=document.createElement('li');
   const itemId = list.length ? list.length : 0
-  // console.log(itemId)
   li.setAttribute('id', itemId);
   const textNode=document.createTextNode(newItem);
   li.appendChild(textNode);
-  //*** Adding Strikethrough line to Li ***
+
   const strikeLine=document.createElement('input');
   strikeLine.setAttribute('type', 'checkbox');
-  strikeLine.setAttribute('class', 'checkboxLine')
+  strikeLine.setAttribute('class', 'checkBoxLine')
   strikeLine.addEventListener('click',function(e){
     strikeLine.checked? e.target.parentElement.classList.add('strikeLine'): e.target.parentElement.classList.remove('strikeLine');
   });
   li.appendChild(strikeLine);
-  //*** Adding delete button to Lis ***
+
   const del=document.createElement('button');
+  del.setAttribute('class', 'deleteButton');
   del.appendChild(document.createTextNode('X'));
   li.appendChild(del);
-  //*** Removing items from array ***
+
   del.addEventListener('click',function(e){
     const liDel=document.getElementById('items');
     const indexItem=list.indexOf(newItem);
-    list.splice(e.target.parentElement.innerText,1)
-    console.log(list)
-    // console.log(e)
-    console.log(e.target.parentElement.innerText)
+    list.splice(indexItem, 1)
+    localStorage.setItem('list',JSON.stringify(list))
     liDel.removeChild(li);
   })
+
   document.getElementById('items').appendChild(li);
   list.push(newItem);
   localStorage.setItem('list', JSON.stringify(list));
 }
-// 
-// function deleteTask(id){
-//   const list=JSON.parse(localStorage.getItem('list')) || itemsArray;
-//   const itemId = list.length ? list.length : 0
-//   for(let i=0; i<list.length; i++){
-//     if(list[i].id == itemId){
-//       list.splice(i,1);
-//     }
-//     localStorage.setItem('list', JSON.stringify(list));
-//     console.log(list);
-//   }
-// }
 
 let displayArrayItems = [];
-//*** Adding items ***
 const savedItems = JSON.parse(localStorage.getItem('list')) || displayArrayItems;
 for (let i=0; i<savedItems.length; i++){
   const displayItems = savedItems[i]
@@ -61,21 +47,25 @@ for (let i=0; i<savedItems.length; i++){
   const li=document.createElement('li');
   const textNode=document.createTextNode(newItem);
   li.appendChild(textNode);
-  //*** Adding strikethrough line to items ***
+
   const strikeLine=document.createElement('input');
   strikeLine.setAttribute('type', 'checkbox');
-  strikeLine.setAttribute('class', 'checkboxLine')
+  strikeLine.setAttribute('class', 'checkBoxLine')
   strikeLine.addEventListener('click',function(e){
     strikeLine.checked? e.target.parentElement.classList.add('strikeLine'): e.target.parentElement.classList.remove('strikeLine');
   });
   li.appendChild(strikeLine);
-  //*** Adding delete button to items ***
+
   const del=document.createElement('button');
+  del.setAttribute('class', 'deleteButton');
   del.appendChild(document.createTextNode('X'));
   li.appendChild(del);
-  //*** Gets rid of items - Returns disappeared items after reload and allows to you to remove it again ***
+
   del.addEventListener('click',function(e){
     const liDel=document.getElementById('items');
+    const indexItem=list.indexOf(newItem);
+    list.splice(indexItem, 1)
+    localStorage.setItem('list',JSON.stringify(list))
     liDel.removeChild(li);
   })
   document.getElementById('items').appendChild(li);
